756dc4e4088a432d2d1f444bc6a138178f57b97c
Sirilert Thitikul <rubii@Rubii-Mac.local>
2022-11-06 23:10:53 +0700

    add docstring and fix some bug

--- chatapp.py
+++ chatapp.py
@@ -1,10 +1,15 @@
 import json
+from sys import exit
 from color import bcolors
 from getdata import DataHandler
 
 
 def Menu():
-	MENU = """
+    """
+    It prints the menu and returns the user's choice
+    :return: The menu is being returned.
+    """
+    MENU = """
 Action
 1. Initialize data
 2. getRoomById
@@ -13,69 +18,78 @@ Action
 5. getAllChatInRoom
 0. Exit
 
-	"""
+    """
 
-	print(MENU)
-	action = int(input("Select the action: "))
-
-	return action
+    print(MENU)
+    return int(input("Select the action: "))
 
 
 def init_data():
-	path = input("Initialize data with file path (default: ChatAppRawDataJSON.json): ")
-	if path == "":
-		path = "ChatAppRawDataJSON.json"
-	try:
-		with open(path) as f:
-			data = json.load(f)
-			print("Initialize...")
+    """
+    It takes a file path as input, and returns a dictionary of the data in the file
+    :return: The data is being returned.
+    """
+    path = input("Initialize data with file path (default: ChatAppRawDataJSON.json): ")
+    if path == "":
+        path = "ChatAppRawDataJSON.json"
+    try:
+        with open(path, encoding='utf-8') as f:
+            data = json.load(f)
+            print("Initialize...")
 
-			return data
-	except json.decoder.JSONDecodeError:
-		print(bcolors.FAIL + "Invalid JSON file" + bcolors.ENDC)
+            return data
+    except json.decoder.JSONDecodeError:
+        print(bcolors.FAIL + "Invalid JSON file" + bcolors.ENDC)
 
 
-def check_action(action, data, cmd):
-	if action == 2:
-		cmd.getRoomById(data)
-	elif action == 3:
-		cmd.getAllRoom(data)
-	elif action == 4:
-		cmd.getChatbyId(data)
-	elif action == 5:
-		cmd.getAllChatInRoom(data)
-	else:
-		print(bcolors.WARNING + "Invalid action" + bcolors.ENDC)
+def check_action(action: int, data: list, cmd: DataHandler):
+    """
+    It checks the action and calls the appropriate function
+    
+    :param action: The action to be performed
+    :param data: The data that is sent to the server
+    :param cmd: The command object that you want to use
+    """
+    if action == 2:
+        cmd.getRoomById(data)
+    elif action == 3:
+        cmd.getAllRoom(data)
+    elif action == 4:
+        cmd.getChatbyId(data)
+    elif action == 5:
+        cmd.getAllChatInRoom(data)
+    else:
+        print(bcolors.WARNING + "Invalid action" + bcolors.ENDC)
 
 
 def main():
-	action = 1
-	data = None
-	while (action != 0):
-		try:
-			action = Menu()
-		except ValueError:
-			print(bcolors.WARNING + "Invalid action" + bcolors.ENDC)
-			continue
+    action = 1
+    data = None
+    while action != 0:
+        try:
+            action = Menu()
+        except ValueError:
+            print(bcolors.WARNING + "Invalid action" + bcolors.ENDC)
+            continue
 
-		cmd = DataHandler()
-		if action == 0:
-			print(bcolors.BOLD + bcolors.OKCYAN + "Bye" + bcolors.ENDC)
-			exit(0)
-		elif action == 1:
-			if data is not None:
-				print(bcolors.WARNING + "Data is already initialized" + bcolors.ENDC)
-				continue
-			try:
-				data = init_data()
-			except FileNotFoundError:
-				print(bcolors.FAIL + "File not found" + bcolors.ENDC)
-		else:
-			if data == None:
-				print(bcolors.FAIL + "Please initialize data first" + bcolors.ENDC)
-			else:
-				check_action(action, data, cmd)
+        cmd = DataHandler()
+        if action == 0:
+            print(bcolors.BOLD + bcolors.OKCYAN + "Bye" + bcolors.ENDC)
+            exit(0)
+        if action == 1:
+            if data is not None:
+                print(bcolors.WARNING + "Data is already initialized" + bcolors.ENDC)
+                continue
+            try:
+                data = init_data()
+            except FileNotFoundError:
+                print(bcolors.FAIL + "File not found" + bcolors.ENDC)
+        else:
+            if data is None:
+                print(bcolors.FAIL + "Please initialize data first" + bcolors.ENDC)
+            else:
+                check_action(action, data, cmd)
 
 
 if "__main__" == __name__:
-	main()
+    main()
--- getdata.py
+++ getdata.py
@@ -1,55 +1,81 @@
 from color import bcolors
 
 
+# > This class is used to handle data from json file
 class DataHandler():
-	def getRoomById(self, data):
-		try:
-			room_id = int(input("getRoomById: "))
-			if room_id <= 0:
-				raise ValueError
-		except ValueError:
-			print(bcolors.FAIL + "Invalid input" + bcolors.ENDC)
-			return
+    def getRoomById(self, data: list):
+        """
+        It takes a list of dictionaries, and prints the dictionary with the key 'id'
+        that matches the user input
 
-		for room in data:
-			if room['id'] == room_id:
-				print({"id": room_id, "name": room["name"]})
-				return
-		print(bcolors.FAIL + f"No Room with id: {room_id}" + bcolors.ENDC)
+        :param data: The data that is passed to the function
+        :return: A list of dictionaries
+        """
+        try:
+            room_id = int(input("getRoomById: "))
+            if room_id <= 0:
+                raise ValueError
+        except ValueError:
+            print(bcolors.FAIL + "Invalid input" + bcolors.ENDC)
+            return
 
-	def getAllRoom(self, data):
-		rooms = []
-		for room in data:
-			rooms.append({"id": room['id'], "name": room["name"]})
-		print(rooms)
+        for room in data:
+            if room['id'] == room_id:
+                print({"id": room_id, "name": room["name"]})
+                return
+        print(bcolors.FAIL + f"No Room with id: {room_id}" + bcolors.ENDC)
 
-	def getChatbyId(self, data):
-		try:
-			chat_id = int(input("getChatbyId: "))
-			if chat_id <= 0:
-				raise ValueError
-		except ValueError:
-			print(bcolors.FAIL + "Invalid input" + bcolors.ENDC)
-			return
+    def getAllRoom(self, data: list):
+        """
+        It takes in a list of dictionaries, and returns a list of dictionaries
 
-		for room in data:
-			for chat in room['chats']:
-				if chat['id'] == chat_id:
-					print({"id": chat_id, "message": chat["message"]})
-					return
-		print(bcolors.FAIL + f"No Chat with id: {chat_id}" + bcolors.ENDC)
+        :param data: The data that is returned from the API call
+        """
+        rooms = []
+        for room in data:
+            rooms.append({"id": room['id'], "name": room["name"]})
+        print(rooms)
 
-	def getAllChatInRoom(self, data):
-		try:
-			room_id = int(input("getAllChatInRoom: "))
-			if room_id <= 0:
-				raise ValueError
-		except ValueError:
-			print(bcolors.FAIL + "Invalid input" + bcolors.ENDC)
-			return
+    def getChatbyId(self, data: list):
+        """
+        It takes in a list of dictionaries, and prints out the message of the chat with the given id
 
-		for room in data:
-			if room['id'] == room_id:
-				print(room['chats'])
-				return
-		print(bcolors.FAIL + f"No Room with id: {room_id}" + bcolors.ENDC)
+        :param data: The data that is passed to the function
+        :return: A dictionary with the id and message of the chat with the given id.
+        """
+        try:
+            chat_id = int(input("getChatbyId: "))
+            if chat_id <= 0:
+                raise ValueError
+        except ValueError:
+            print(bcolors.FAIL + "Invalid input" + bcolors.ENDC)
+            return
+
+        for room in data:
+            for chat in room['chats']:
+                if chat['id'] == chat_id:
+                    print({"id": chat_id, "message": chat["message"]})
+                    return
+        print(bcolors.FAIL + f"No Chat with id: {chat_id}" + bcolors.ENDC)
+
+    def getAllChatInRoom(self, data: list):
+        """
+        It takes a list of rooms, and prints all the chats in a room with a given id
+
+        :param data: list
+        :type data: list
+        :return: A list of all the chats in the room with the given id.
+        """
+        try:
+            room_id = int(input("getAllChatInRoom: "))
+            if room_id <= 0:
+                raise ValueError
+        except ValueError:
+            print(bcolors.FAIL + "Invalid input" + bcolors.ENDC)
+            return
+
+        for room in data:
+            if room['id'] == room_id:
+                print(room['chats'])
+                return
+        print(bcolors.FAIL + f"No Room with id: {room_id}" + bcolors.ENDC)
